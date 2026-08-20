67fc4a097904651cf684983fa01db392fda2f204
leirbga <smortad.legedary@gmail.com>
2026-08-20 19:13:15 -0400

    correciones

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// import app from './app.js';
-// import http from 'http';
+import app from './app.js';
+import http from 'http';
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
-// const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3003;
 
-// server.listen(PORT, () => {
-//     console.log(`🚀 El servidor está ejecutándose en el puerto http://localhost:${PORT}`);
-// });
+server.listen(PORT, () => {
+     console.log(`🚀 El servidor está ejecutándose en el puerto http://localhost:${PORT}`);
+});
